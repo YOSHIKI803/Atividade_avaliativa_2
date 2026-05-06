739c9890ef2157fa2e5faeac68cd183b48bbefd6
Lucas Pereira <lucas1jp2.com@gmail.com>
2026-05-05 21:57:06 -0400

    TAREFA NÃO CONCLUIDO

--- AppAtividade2/Formularios/FormJurosSimples.Designer.cs
+++ AppAtividade2/Formularios/FormJurosSimples.Designer.cs
@@ -29,11 +29,214 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.txtCapital = new System.Windows.Forms.TextBox();
+            this.txtTaxa = new System.Windows.Forms.TextBox();
+            this.txtTempo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtMontante = new System.Windows.Forms.TextBox();
+            this.txtJuros = new System.Windows.Forms.TextBox();
+            this.ComboBox = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(426, 618);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(198, 61);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "CALCULAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(681, 617);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(198, 61);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "LIMPAR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // txtCapital
+            // 
+            this.txtCapital.Location = new System.Drawing.Point(706, 284);
+            this.txtCapital.Name = "txtCapital";
+            this.txtCapital.Size = new System.Drawing.Size(173, 26);
+            this.txtCapital.TabIndex = 3;
+            this.txtCapital.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtTaxa
+            // 
+            this.txtTaxa.Location = new System.Drawing.Point(706, 365);
+            this.txtTaxa.Name = "txtTaxa";
+            this.txtTaxa.Size = new System.Drawing.Size(173, 26);
+            this.txtTaxa.TabIndex = 4;
+            // 
+            // txtTempo
+            // 
+            this.txtTempo.Location = new System.Drawing.Point(706, 454);
+            this.txtTempo.Name = "txtTempo";
+            this.txtTempo.Size = new System.Drawing.Size(173, 26);
+            this.txtTempo.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(419, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(447, 37);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "CALCULADORA DE JUROS";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(422, 287);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Capital:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(422, 365);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(155, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Taxa (porcentagem):";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(422, 454);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(123, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Tempo (meses):";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(422, 545);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 20);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Juros:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(422, 205);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(81, 20);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Montante:";
+            // 
+            // txtMontante
+            // 
+            this.txtMontante.BackColor = System.Drawing.Color.LawnGreen;
+            this.txtMontante.Location = new System.Drawing.Point(706, 199);
+            this.txtMontante.Name = "txtMontante";
+            this.txtMontante.Size = new System.Drawing.Size(173, 26);
+            this.txtMontante.TabIndex = 12;
+            // 
+            // txtJuros
+            // 
+            this.txtJuros.BackColor = System.Drawing.Color.LawnGreen;
+            this.txtJuros.Location = new System.Drawing.Point(706, 539);
+            this.txtJuros.Name = "txtJuros";
+            this.txtJuros.Size = new System.Drawing.Size(173, 26);
+            this.txtJuros.TabIndex = 14;
+            // 
+            // ComboBox
+            // 
+            this.ComboBox.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.ComboBox.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.ComboBox.FormattingEnabled = true;
+            this.ComboBox.Items.AddRange(new object[] {
+            "CALCULAR O MONTANTE SIMPLES",
+            "CALCULAR O CAPITAL",
+            "CALCULAR A TAXA",
+            "CALCULAR O TEMPO"});
+            this.ComboBox.Location = new System.Drawing.Point(426, 126);
+            this.ComboBox.Name = "ComboBox";
+            this.ComboBox.Size = new System.Drawing.Size(424, 28);
+            this.ComboBox.TabIndex = 15;
+            this.ComboBox.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(421, 98);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(333, 25);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Selecione o que desejar calcular:";
+            // 
+            // FormJurosSimples
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1308, 747);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.ComboBox);
+            this.Controls.Add(this.txtJuros);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtMontante);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTempo);
+            this.Controls.Add(this.txtTaxa);
+            this.Controls.Add(this.txtCapital);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "FormJurosSimples";
             this.Text = "FormJurosSimples";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox txtCapital;
+        private System.Windows.Forms.TextBox txtTaxa;
+        private System.Windows.Forms.TextBox txtTempo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtMontante;
+        private System.Windows.Forms.TextBox txtJuros;
+        private System.Windows.Forms.ComboBox ComboBox;
+        private System.Windows.Forms.Label label7;
     }
 }
--- AppAtividade2/Formularios/FormJurosSimples.cs
+++ AppAtividade2/Formularios/FormJurosSimples.cs
@@ -16,5 +16,61 @@ namespace AppAtividade2.Formularios
         {
             InitializeComponent();
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btLimpar_Click(object sender, EventArgs e)
+        {
+            txtCapital.Clear();
+            txtTaxa.Clear();
+            txtTempo.Clear();
+            txtCapital.Focus();
+        }
+
+        private void textBox1_BackColorChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int tipoCalculo = cbTipoCalculo.SelectedIndex;
+            double taxa = txtTaxa.Text != "" ? Convert.ToDouble(txtTaxa.Text) / 100 : 0;
+            double capital = Convert.ToDouble(txtCapital.Text);
+            int tempo = Convert.ToInt32(txtTempo.Text);
+            double juros = 0, montante = 0;
+            
+            montante = capital * (1 + taxa * tempo);
+
+            txtMontante.Text = montante.ToString("C2");
+
+            juros = montante - capital;
+
+            txtJuros.Text = juros.ToString("C2");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            InitializeComponent();
+            
+        }
     }
 }
